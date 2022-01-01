f98e87689ce3211f92a94c9f58e83e549eb2e224
Alek Michelson <am7590@rit.edu>
2021-12-31 19:43:05 -0500

    Modified the JSON dictionary keys

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ from scrape_new_posts import *
 app = Flask(__name__)
 
 
-# Default GET call
+# Default GET
 @app.route('/', methods=['GET'])
 def home_page():
-    data_set = {'Type of post': 'None', 'Content': 'Loaded the WSB Ticker API', 'Timestamp': time.time()}
+    data_set = {'Type': 'None', 'Content': 'Loaded the WSB Ticker API', 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
 
 
-# Hot Posts GET call
+# Hot Posts GET
 # Ex. Get 100 hottest posts: /hot/?hot=100/
 @app.route('/hot/', methods=['GET'])
 def hot_posts():
     user_query = int(request.args.get('hot'))
 
     h_posts = scrape_hot_posts(user_query, 'wallstreetbets')
-    data_set = {'Type of post': 'Hot', 'Number of posts scraped': user_query, 'Content': h_posts,
+    data_set = {'Type': 'Hot', 'Scraped': user_query, 'Content': h_posts,
                 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
 
@@ -37,7 +37,7 @@ def new_posts():
     user_query = int(request.args.get('new'))
 
     h_posts = scrape_new_posts(user_query, 'wallstreetbets')
-    data_set = {'Type of post': 'New', 'Number of posts scraped': user_query, 'Content': h_posts,
+    data_set = {'Type': 'New', 'Scraped': user_query, 'Content': h_posts,
                 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
 
@@ -49,13 +49,13 @@ def new_posts():
 @app.route('/24h/', methods=['GET'])
 def get_h_posts():
     h_posts = scrape_24h_posts('wallstreetbets')
-    data_set = {'Type of post': '24h', 'Content': h_posts, 'Timestamp': time.time()}
+    data_set = {'Type': '24h', 'Content': h_posts, 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
 
     return json_dump
 
 
-# Run API
+# Run API on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
